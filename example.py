9e59b57698fa4950670867d9d73d98eb7099a02e
jmldennis <jmldennis@gmail.com>
2022-06-30 10:06:37 -0500

    wrote main function

--- example.py
+++ example.py
@@ -1,1 +1,9 @@
-import requests
+#import requests
+
+#This is my main function
+def main():
+    print("Hello World")
+
+#Run main if python script is main script
+if __name__ == "__main__":
+    main()
